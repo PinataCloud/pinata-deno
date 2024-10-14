876391ff01b3f5e40c1ec2d071c68a416e8a0921
Steve <stevedsimkins@gmail.com>
2024-10-14 10:30:22 -0400

    Removed comments

--- main.ts
+++ main.ts
@@ -3,10 +3,8 @@ import { pinata } from "./pinata.ts";
 async function handler(req: Request): Promise<Response> {
 	const path = new URL(req.url).pathname;
 
-	// respond with text/html
 	if (path === "/") return new Response("Welcome to Deno!");
 
-	// receive POST data from a form
 	if (req.method === "POST" && path === "/upload") {
 		const data = await req.formData();
 		const file = data.get("file") as File;
@@ -17,5 +15,4 @@ async function handler(req: Request): Promise<Response> {
 	return new Response("Path not found", { status: 404 });
 }
 
-// To start the server on the default port, call `Deno.serve` with the handler.
 Deno.serve(handler);
